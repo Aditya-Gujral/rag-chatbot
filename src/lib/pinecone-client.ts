4f40390cf6469f99c7fcfa922b8206aeb5f291dc
Aditya-Gujral <69429976+Aditya-Gujral@users.noreply.github.com>
2024-10-22 07:11:19 -0400

    Update pinecone-client.ts

--- src/lib/pinecone-client.ts
+++ src/lib/pinecone-client.ts
@@ -3,7 +3,7 @@ import { env } from "./config";
 import { delay } from "./utils";
 
 // Declare the pineconeClientInstance variable at a higher scope
-let pineconeClientInstance: Pinecone();
+let pineconeClientInstance: Pinecone() | null=null;
 
 // Initialize index and ready to be accessed.
 async function initPineconeClient() {
