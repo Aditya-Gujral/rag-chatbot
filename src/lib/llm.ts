748f661d6532c6306fb16b62de327a34d9ad96fe
Aditya-Gujral <69429976+Aditya-Gujral@users.noreply.github.com>
2024-10-22 19:25:40 -0400

    Update llm.ts

--- src/lib/llm.ts
+++ src/lib/llm.ts
@@ -1,18 +1,12 @@
-import {
-  BaseLanguageModel,
-  BaseLanguageModelInterface,
-  BaseLanguageModelInput,
-} from "@langchain/core/language_models/base";
-
 import { ChatOpenAI } from "@langchain/openai";
 
-export const streamingModel: BaseLanguageModel = new ChatOpenAI({
+export const streamingModel= new ChatOpenAI({
   modelName: "gpt-3.5-turbo",
   streaming: true,
   temperature: 0,
 });
 
-export const nonStreamingModel: BaseLanguageModel = new ChatOpenAI({
+export const nonStreamingModel= new ChatOpenAI({
   modelName: "gpt-3.5-turbo",
   temperature: 0,
 });
